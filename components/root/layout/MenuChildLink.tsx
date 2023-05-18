30066f18faeb0110d177585a16de67ced7af9dfc
dmoreno <david.mesm@gmail.com>
2023-05-18 13:07:13 -0600

    initial commit

---
+++ components/root/layout/MenuChildLink.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { route } from "@/config/route.config";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState, useEffect, useRef } from "react";
+import styles from "./MenuLink.module.scss";
+
+export interface IMenuChildLinkProps {
+  parentTo: string;
+  route: route;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function MenuChildLink(props: IMenuChildLinkProps) {
+  const { parentTo, route, setOpen } = props;
+
+  const pathname = usePathname();
+
+  const [active, setActive] = useState(false);
+
+  const childRef = useRef<HTMLLIElement>(null)
+
+  useEffect(() => {
+    setActive(pathname.startsWith(parentTo + route.to))
+  }, [parentTo, pathname, route, setActive]);
+
+  useEffect(() => {
+    if(active) {
+      setOpen(true)
+      if(childRef.current) {
+        childRef.current.focus()
+        childRef.current.scrollIntoView()
+      }
+    }
+  }, [active, setOpen])
+  
+
+  return (
+    <li ref={childRef} className={`${styles.menuChildItem} ${active ? styles.selected : ""}`}>
+      <div className={styles.menuChildLink}>
+        <Link href={`${parentTo}${route.to}`}>{route.label}</Link>
+      </div>
+    </li>
+  );
+}
